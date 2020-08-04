7e39fb5a07b82708395fa32ab911d01ba69e5358
Horus125 <ignacykuchcinski@gmail.com>
2020-08-04 22:31:52 +0000

    Update GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "81356199e46a43ba9d14d7c7b0d7ff9c",
+  "main.dart.js": "77fdaf2392617bd83582da5040f058f5",
 "favicon.png": "0bbb275c3be676f24451e7680147c8a2",
 "index.html": "b28a4fa667aecc6a81ce7156a87c6bca",
 "/": "b28a4fa667aecc6a81ce7156a87c6bca",
