2a273eb136d10f8bae4500cbc0eafb0fefd83d29
dahlia-os <contact@dahliaos.io>
2020-08-22 23:20:44 +0000

    Update GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "icons/Icon-192.png": "0bbb275c3be676f24451e7680147c8a2",
 "icons/Icon-512.png": "0bbb275c3be676f24451e7680147c8a2",
 "favicon.png": "0bbb275c3be676f24451e7680147c8a2",
-"main.dart.js": "918b62993b00a01bce9a1db11fc488e1",
+"main.dart.js": "0a852942a9dad06586baf7f114434603",
 "index.html": "b28a4fa667aecc6a81ce7156a87c6bca",
 "/": "b28a4fa667aecc6a81ce7156a87c6bca",
 "manifest.json": "8ff5fe849411d088c5084cdf099d07cf",
