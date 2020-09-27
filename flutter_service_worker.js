322257e1eea28dfcaf3928997d7271225d037995
dahlia-os <contact@dahliaos.io>
2020-09-27 16:59:06 +0000

    Update GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -118,8 +118,8 @@ const RESOURCES = {
 "assets/assets/images/icons/v2/compiled/Gmail-icon.png": "dfcbd586c56f73e343b25b3af12491c1",
 "assets/AssetManifest.json": "bfbba6bb6f5f89bd8096e53999f9c87d",
 "favicon.png": "02f1c9f097b0a80bf77544d0a382d990",
-"index.html": "aaca2d1d9a3dbce8696d169443a426cc",
-"/": "aaca2d1d9a3dbce8696d169443a426cc",
+"index.html": "c557e18fba3bba1c1bf0d902af0d8a12",
+"/": "c557e18fba3bba1c1bf0d902af0d8a12",
 "manifest.json": "8ff5fe849411d088c5084cdf099d07cf",
 "icons/Icon-512.png": "0bd5aff94e7ef56773d3cb06d74973be",
 "icons/Icon-192.png": "16ad25f94af6006cb717fa5552eadcfc"
